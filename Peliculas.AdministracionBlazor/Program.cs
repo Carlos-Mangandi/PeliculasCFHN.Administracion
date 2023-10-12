ed29f7951d4d9dfed684b5df00187b84b7f3dc85
Rafa-X0 <rafaelhernandez.gz@gmail.com>
2023-10-12 12:00:06 -0600

    Codificacion de las acciones Agregar, Editar, Eliminar y Index de Rol

--- Peliculas.AdministracionBlazor/Program.cs
+++ Peliculas.AdministracionBlazor/Program.cs
@@ -1,6 +1,8 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using Peliculas.AdministracionBlazor;
 using Peliculas.AdministracionBlazor.Auth;
 
@@ -10,8 +12,11 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddMudServices();
+
 builder.Services.AddScoped<UserAuth>();
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationProvider>();
 builder.Services.AddAuthorizationCore();
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
